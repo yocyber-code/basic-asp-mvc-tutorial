eaed26e4b0d5a2a93429bf464b8ffc067913e92a
yocyber-code <yocyberyoshi@gmail.com>
2023-09-17 01:20:46 +0700

    'list'

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BasicASPTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BasicASPTutorial.Controllers
 {
@@ -6,7 +7,25 @@ namespace BasicASPTutorial.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<Student> students = new List<Student>();
+            var student1 = new Student();
+            student1.Id = 1;
+            student1.Name = "John";
+            student1.Score = 100;
+            var student2 = new Student();
+            student2.Id = 2;
+            student2.Name = "Jame";
+            student2.Score = 49;
+            var student3 = new Student();
+            student3.Id = 3;
+            student3.Name = "Jane";
+            student3.Score = 97;
+
+            students.Add(student1);
+            students.Add(student2);
+            students.Add(student3);
+
+            return View(students);
         }
         
         public IActionResult Create()
---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicASPTutorial.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
